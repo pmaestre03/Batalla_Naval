e65f5bebc106ea253eb7e2eb3f22404981102574
pmaestre03 <maestrep15@gmail.com>
2023-09-22 17:33:41 +0200

    Matriz hecha

--- BatallaNaval.php
+++ BatallaNaval.php
@@ -6,27 +6,37 @@
         border-collapse: collapse;
         text-align: center;
         width: 25px;
-        
     }
 </style>
 
 <table>
 <?php
-$submarino = [['J',10],['J',9]];
+$submarino = [[['J',10],['J',9]]];
+$matriu = [];
+$partida = [];
 $n=10;
 $alpha=range("A","Z");
-var_dump(count($submarino));
+//var_dump($submarino);
+for ($i=0;$i<$n;$i++) {
+    for ($x=0;$x<$n;$x++) {
+        $matriu[$i][$x] = "0";
+        $partida[$i][$x] = "0";
+    }
+}
+
+//var_dump($matriu);
+
 for ($i=0;$i<=$n;$i++) {
     echo "<tr>\n";
     echo "<td>".$alpha[$i-1]."</td>\n";
     for ($x=1;$x<=$n;$x++) {
-        //echo "<td> x=$x i=$i </td>\n";
         if ($i==0) {
             echo "<td>$x</td>";
         }
         else {
             if ($submarino[0][0]==$alpha[$i-1] && $submarino[0][1]==$x) {
-                echo "<td>S</td>";
+                $partida[$i-1][$x] = "S";
+                echo "<td style='background-color:grey;'>S</td>";
             }
             else {
                 echo "<td style='background-color:blue;'></td>";
@@ -35,5 +45,6 @@ for ($i=0;$i<=$n;$i++) {
     }
     echo "</tr>\n";
 }
+var_dump($partida[10-1]);
 ?>
 </table>
