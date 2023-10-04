a1c29d48cb89fbfb132729a2bffafa2776e37e97
¨pmaestre03¨ <maestrep15@gmail.com>
2023-10-04 12:21:32 +0200

    Todos los barcos

--- BatallaNaval.php
+++ BatallaNaval.php
@@ -20,25 +20,7 @@ $partida = [];
 $posiciones = false;
 $n=10;
 $alpha=range("A","Z");
-//var_dump($submarino);
-for ($i=0;$i<$n;$i++) {
-    for ($x=0;$x<$n;$x++) {
-        $matriu[$i][$x] = "0";
-        $partida[$i][$x] = "0";
-    }
-}
-var_dump($partida);
-while ($posiciones==false) {
-    for ($i=0;$i<3;$i++) {
-        $columna = rand(1,$n-1);
-        $fila = rand(1,$n-1);
-        
-        array_push($submarino,[$alpha[$columna],$fila]);
-    }
-    $posiciones =true;
-}
-
-var_dump($submarino);
+var_dump(count($submarino));
 for ($i=0;$i<=$n;$i++) {
     echo "<tr>\n";
     echo "<td>".$alpha[$i-1]."</td>\n";
@@ -47,10 +29,8 @@ for ($i=0;$i<=$n;$i++) {
             echo "<td>$x</td>";
         }
         else {
-
-            if ($submarino[1][0]==$alpha[$i-1] && $submarino[1][1]==$x) {
-                $partida[$i-1][$x] = "S";
-                echo "<td style='background-color:grey;'>S</td>";
+            if ($submarino[0][0]==$alpha[$i-1] && $submarino[0][1]==$x) {
+                echo "<td>S</td>";
             }
             else {
                 echo "<td style='background-color:blue;'></td>";
